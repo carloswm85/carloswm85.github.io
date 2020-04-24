4f6e93cb0b590e10bc74279d68d5aa7d49a573d5
carloswm85 <carloswashingtonmercado@gmail.com>
2020-04-24 06:50:08 -0300

    Added date to the JS file.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-function lastUpdated() {
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  };
+const options = {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+};
 
-  document.getElementById('currentDate').textContent = new Date(document.lastModified).toLocaleDateString('en-US'. options);
-}
+let last = document.lastModified;
+let date = new Date(last);
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let local = date.toLocaleDateString('en-US'. options);
+let fullDate = `${local} ${hours}:${minutes}:${seconds}`;
+
+document.getElementById('currentDate').textContent = fullDate;
+
