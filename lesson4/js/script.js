b3a32e5b9712ab2e34588ca65a969c3f618bef42
carloswm85 <carloswashingtonmercado@gmail.com>
2020-05-16 17:45:41 -0300

    Finished Preston Site

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -7,16 +7,18 @@ const options = {
   year: 'numeric'
 };
 
+options.timeZoneName ='short';
+
 let last = document.lastModified;
 let date = new Date(last);
+
 let hours = date.getHours();
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
-let local = date.toLocaleDateString('en-US'. options);
+let local = date.toLocaleString('en-US', options);
 let fullDate = `${local} ${hours}:${minutes}:${seconds}`;
 
 document.getElementById('currentDate').textContent = fullDate;
-
 
 // HAMBURGER MENU
 
