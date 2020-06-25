9bef3bc2cb8f3eddc0e9d76cc17494f46a78ed61
carloswm85 <carloswashingtonmercado@gmail.com>
2020-06-25 16:40:24 -0300

    Adding a lot of stuffs for w10

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,60 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=f4051f9ae5c7a4eb58c335ed524c93c6';
+const apiURLforecast = 'https://api.openweathermap.org/data/2.5/onecall?lat=42.1&lon=-111.88&exclude=current,minutely,hourly&appid=f4051f9ae5c7a4eb58c335ed524c93c6';
+
+// SUMMARY SECTION
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.table(jsObject);
+    //console.log(jsObject);
+
+    const currently = jsObject.weather[0].description;
+    const temp_high = jsObject.main.temp_max;
+    const windchill = 0.0;
+    const humidity = jsObject.main.humidity;
+    const windSpeed = jsObject.wind.speed;
+
+    //console.log(currently, temp_high, windchill, humidity, windSpeed)
+
+    if (temp_high < 50 && windSpeed > 3) {
+        windChill = 35.74 + 0.6215 * temp_high - 35.75 * windSpeed ** 0.16 + 0.4275 * temp_high * windSpeed ** 0.16;
+        windChill = Math.round(windChill * 10) / 10;
+        //console.log(windChill);
+        windChill_display = windChill + ' &#8457;';
+    } else {
+        windChill_display = 'N/A';
+    }
+
+    document.getElementById('weather-preston-1').innerHTML = currently;
+    document.getElementById('weather-preston-2').innerHTML = temp_high;
+    document.getElementById('weather-preston-3').innerHTML = windChill_display;
+    document.getElementById('weather-preston-4').innerHTML = humidity;
+    document.getElementById('weather-preston-5').innerHTML = windSpeed;
+  });
+
+// FORECAST SECTION
+fetch(apiURLforecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.table(jsObject);
+    console.log(jsObject);
+
+    for (let i = 1; i < 6; i++) {
+      /*document.getElementById('forecast-preston-' + i).innerHTML = jsObject.daily[i].temp.eve;
+      const imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+
+      document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+      document.getElementById('icon').setAttribute('alt', desc);*/
+    }
+  });
+
+/*
+<tr class="table-row">
+  <td class="table-cell">Forecast</td>
+  <td class="table-cell"><span id="forecast-preston-1"></span> &#8457; </td>
+  <td class="table-cell"><span id="forecast-preston-2"></span> &#8457; </td>
+  <td class="table-cell"><span id="forecast-preston-3"></span> &#8457; </td>
+  <td class="table-cell"><span id="forecast-preston-4"></span> &#8457; </td>
+  <td class="table-cell"><span id="forecast-preston-5"></span> &#8457; </td>
+</tr>
+*/  
