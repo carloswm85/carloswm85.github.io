a6e192eb98e1b0de00b3047de2c13792f1bfd55c
carloswm85 <carloswashingtonmercado@gmail.com>
2020-07-21 17:25:10 -0300

    New commit

--- lesson13/js/rentals.js
+++ lesson13/js/rentals.js
@@ -1,13 +1,27 @@
-const rentalsJSON = "../data/rentals.json";
+const rentalsJSON = "https://carloswm85.github.io/lesson13/data/rentals.json";
+const parsedJSON = JSON.parse(rentalsJSON);
 
-console.log(typeof(rentalsJSON));
+console.log(typeof(parsedJSON));
+console.log(parsedJSON);
 
-fetch(JSONrentals)
+fetch(parsedJSON)
 .then((response) => response.json())
 .then((jsObject) => {
   
-  let current = jsObject.rentals[0].type;
-  console.log(current);
-    
-  document.getElementById('data').innerHTML = current;
+  document.getElementById('type').innerHTML = jsObject.rentals[0].type;
+
+  let test1 = jsObject.rentals[0].capacity;
+  document.getElementById('capacity').textContent = test1;
+  console.log(test1);
+
+  let test2 = jsObject.rentals[0].reservationHalf;
+  document.getElementById('reservation-half').textContent = test2;
+  console.log(test2);
+  console.log(typeof(test2));
+
+  document.getElementById('reservation-full').textContent = jsObject.rentals[0].reservationFull;
+
+  document.getElementById('walkin-half').innerHTML = jsObject.rentals[0].walkinHalf;
+
+  document.getElementById('walkin-full').innerHTML = jsObject.rentals[0].walkinFull;
 });
