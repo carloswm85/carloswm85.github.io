578e8d860f88b4c1111ea764bc21bd8c06eea764
carloswm85 <carloswashingtonmercado@gmail.com>
2020-09-09 11:34:38 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
+// SIDE NAVIGATION BAR
+function openSideNav() {
 	document.getElementById("sideNav").style.width = "250px";
+	document.getElementById("header").style.marginLeft = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("footer").style.marginLeft = "250px";
 	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
+function closeSideNav() {
 	document.getElementById("sideNav").style.width = "0";
+	document.getElementById("header").style.marginLeft= "0";
+	document.getElementById("main").style.marginLeft= "0";
+	document.getElementById("footer").style.marginLeft= "0";
 	document.body.style.backgroundColor = "white";
-} 
+}
