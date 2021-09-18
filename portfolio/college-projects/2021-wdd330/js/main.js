b0e13a3b5ab6a427ef62fc8839e64f644966088f
carloswm85 <carloswashingtonmercado@gmail.com>
2021-09-17 21:08:15 -0300

    add wdd330 2021 Fall class project folder

---
+++ portfolio/college-projects/2021-wdd330/js/main.js
@@ -0,0 +1,36 @@
+const list = document.querySelector('#list');
+const links = [
+	{
+		label: 'Week 01',
+		url: 'week01/index.html'
+	},
+	{
+		label: 'Week 02',
+		url: 'week02/index.html'
+	},
+	{
+		label: 'Week 03',
+		url: 'week03/index.html'
+	},
+	// {
+	// 	label: 'Week 0',
+	// 	url: 'week0/index.html'
+	// },
+]
+
+links.forEach(element => {
+	var listItem = document.createElement('li');
+	var linkElement = document.createElement('a');
+	var span = document.createElement('span');
+	
+	linkElement.href = element.url;
+	linkElement.innerText = element.label;
+	linkElement.target = '_blank';
+
+	// &nearr; &UpperRightArrow; &nearrow; &#x02197; &#8599;
+	span.innerHTML = ' &#8663;';
+
+	linkElement.appendChild(span);
+	listItem.appendChild(linkElement);
+	list.appendChild(listItem);
+});
