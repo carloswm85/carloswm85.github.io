38517cd5a623ce023e71e978463dd4895f3c9411
carloswm85 <carloswashingtonmercado@gmail.com>
2021-09-26 02:27:24 -0300

    Add team acti

--- portfolio/college-projects/2021-wdd330/week02/script-team.js
+++ portfolio/college-projects/2021-wdd330/week02/script-team.js
@@ -9,3 +9,17 @@ lastEdit.innerText = theDate;
 /** ------------------------------------------------------------------------
  * Team Activity
  --------------------------------------------------------------------------- */
+let button = document.getElementById('button');
+let cont = document.querySelector('.empty-container');
+ 
+button.addEventListener('click', () => {
+	 cont.innerText = addNumbers();	 
+});
+	
+function addNumbers() {
+	let text = parseFloat(document.getElementById('input').value);
+	let text2 = parseFloat(document.getElementById('input2').value);
+	let sum = text + text2;
+	return sum;
+}
+
