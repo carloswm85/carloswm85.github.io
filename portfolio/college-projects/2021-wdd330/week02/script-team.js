cedb2cac87f392b84088314118939466e4ccd003
carloswm85 <carloswashingtonmercado@gmail.com>
2021-09-26 01:10:10 -0300

    add files for team activities

---
+++ portfolio/college-projects/2021-wdd330/week02/script-team.js
@@ -0,0 +1,11 @@
+/**
+ * LAST EDIT
+ */
+const lastEdit = document.querySelector(".last-edit");
+const theDate = new Date(document.lastModified).toLocaleDateString();
+lastEdit.innerText = theDate;
+
+
+/** ------------------------------------------------------------------------
+ * Team Activity
+ --------------------------------------------------------------------------- */
