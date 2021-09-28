73a1481ef7f9ac05c095d7a7c994520515fda886
carloswm85 <carloswashingtonmercado@gmail.com>
2021-09-28 13:22:12 -0300

    add week template folder, add week 03 yet empty

---
+++ portfolio/college-projects/2021-wdd330/week03/script.js
@@ -0,0 +1,12 @@
+/**
+ * LAST EDIT
+ */
+const lastEdit = document.querySelector(".last-edit");
+const theDate = new Date(document.lastModified).toLocaleDateString();
+lastEdit.innerText = theDate;
+
+// READINGS & EXERCISES
+/** ------------------------------------------------------------------------
+ * Book
+ --------------------------------------------------------------------------- */
+
