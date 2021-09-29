ddc2b58d67110bf0f4f7a4c9824cfbf79f3d4518
carloswm85 <carloswashingtonmercado@gmail.com>
2021-09-29 20:40:32 -0300

    add wdd330 empty folder up to w10

--- portfolio/college-projects/2021-wdd330/js/main.js
+++ portfolio/college-projects/2021-wdd330/js/main.js
@@ -13,13 +13,37 @@ const links = [
 		url: 'week02/index.html'
 	},
 	{
-		label: 'Week 03 (try me)',
+		label: 'Week 03',
 		url: 'week03/index.html'
 	},
-	// {
-	// 	label: 'Week 0',
-	// 	url: 'week0/index.html'
-	// },
+	{
+		label: 'Week 04 (try me)',
+		url: 'week04/index.html'
+	},
+	{
+		label: 'Week 05 (try me)',
+		url: 'week05/index.html'
+	},
+	{
+		label: 'Week 06 (try me)',
+		url: 'week06/index.html'
+	},
+	{
+		label: 'Week 07 (try me)',
+		url: 'week04/index.html'
+	},
+	{
+		label: 'Week 08 (try me)',
+		url: 'week08/index.html'
+	},
+	{
+		label: 'Week 09 (try me)',
+		url: 'week09/index.html'
+	},
+	{
+		label: 'Week 10 (try me)',
+		url: 'week10/index.html'
+	}
 ]
 
 links.forEach(element => {
