b824b22f497f7ee792a32d3bc74c3aefc543e60b
carloswm85 <carloswashingtonmercado@gmail.com>
2021-12-01 22:00:19 -0300

    list with sorting working

--- portfolio/college-projects/f_2021-wdd330/projects/project02_binance-orders/src/scripts/main.js
+++ portfolio/college-projects/f_2021-wdd330/projects/project02_binance-orders/src/scripts/main.js
@@ -15,10 +15,12 @@ const v2 = '/v2';
 const v3 = '/v3';
 
 // Endpoints
-const depth = testnet + v3 + '/depth';
-const exchangeInfo = testnet + v3 + '/exchangeInfo';
-const priceStatistics = testnet + v3 + "/ticker/24hr";
-const allPrices = testnet + v1 + '/ticker/allPrices';
+const baseUrl = testnet;
+const depth = `${baseUrl}${v3}/depth`;
+const exchangeInfo = `${baseUrl}${v3}/exchangeInfo`;
+const priceStatistics = `${baseUrl}${v3}/ticker/24hr`;
+const allPrices = `${baseUrl}${v1}/ticker/allPrices`;
+
 
 // Etc
 let symbol = "?symbol=";
@@ -26,21 +28,27 @@ const btc = "BTCUSDT";
 
 async function showList(url) {
 	const data = await getJson(url);
-	const listaSelect = document.getElementById('asset-selection_id');
-	console.log('Show List:');
-	console.log(data);
+	const symbolsList = document.getElementById('asset-selection_id');
+	const symbolsArray = [];
 
 	data.forEach(element => {
 		const symbol = element.symbol;
+		symbolsArray.push(symbol)
+	})
+	
+	symbolsArray.sort();
+
+	symbolsArray.forEach(element => {
+		const symbol = element;
 		const option = document.createElement('option');
 		
 		option.setAttribute('value', symbol);
 		option.innerText = symbol;
-		listaSelect.appendChild(option);
+		symbolsList.appendChild(option);
 	});
 }
 
 
 // displayText();
-const test = '';
-showList(test);
+const test = 'https://carloswm85.github.io/portfolio/college-projects/f_2021-wdd330/projects/project02_binance-orders/src/scripts/allPrices.json';
+showList(allPrices);
