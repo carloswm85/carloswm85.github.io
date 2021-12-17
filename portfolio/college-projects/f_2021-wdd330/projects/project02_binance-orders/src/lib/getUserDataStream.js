62b380973faea3cc9479c8232462afb9bf76b30b
carloswm85 <carloswashingtonmercado@gmail.com>
2021-12-17 19:23:17 -0300

    simple update

--- portfolio/college-projects/f_2021-wdd330/projects/project02_binance-orders/src/lib/getUserDataStream.js
+++
@@ -1,9 +0,0 @@
-// import 'regenerator-runtime/runtime';
-import { spotMarketDataRequest } from './requestClient.js';
-
-export const getUserDataStream = (apiKey) => spotMarketDataRequest(apiKey)('POST', '/api/v3/userDataStream')
-	.post('/api/v3/userDataStream')
-	.then(({
-		data
-	}) => data.listenKey);
-
--- portfolio/college-projects/f_2021-wdd330/projects/project02_binance-orders/src/lib/index.mjs
+++
@@ -1,5 +0,0 @@
-// inside modules/index.mjs:
-export * from './keys.mjs';
-export * from './utilities.mjs';
-export * from './styling.mjs';
-export * from './minichart.mjs';
