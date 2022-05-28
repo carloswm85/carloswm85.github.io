9ffc3395950925eb5bcdca56d0535c93b3afff06
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-05-28 20:24:04 -0300

    simple update

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// Last time page code was updated/changed
+const options = {
+	year: "numeric",
+	month: "long",
+	weekday: "long",
+	day: "numeric",
+	hour: "numeric",
+	minute: "numeric",
+	second: "numeric",
+	hour12: true
+};
+
+let last = document.lastModified;
+let date = new Date(last);
+let local = date.toLocaleDateString("en-US", options);
+let fullDate = `${local}`;
+document.getElementById("updated").textContent = fullDate;
