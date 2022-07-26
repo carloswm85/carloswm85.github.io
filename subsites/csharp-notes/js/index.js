1c239c5deee4d1eb95fa5cbe01ae10cfc777f1b1
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-07-26 14:36:43 -0300

    simple update

--- subsites/csharp-notes/js/index.js
+++ subsites/csharp-notes/js/index.js
@@ -1,3 +1,10 @@
 import { Search } from "./utils.js";
 
 addEventListener("onkeyup", Search);
+
+var tooltipTriggerList = [].slice.call(
+	document.querySelectorAll('[data-bs-toggle="collapse"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	return new bootstrap.Tooltip(tooltipTriggerEl);
+});
