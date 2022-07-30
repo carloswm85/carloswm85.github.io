79dddfc32c6b90837a6d13d7a99ba262c4f300e3
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-07-30 10:14:11 -0300

    simple update

--- subsites/csharp-notes/js/references.js
+++ subsites/csharp-notes/js/references.js
@@ -6,65 +6,12 @@ fetch(requestURL)
 		return response.json();
 	})
 	.then(function (jsonObject) {
-		const books = jsonObject["references"];
+		const references = jsonObject["references"];
 		console.table(jsonObject); // temporary checking for valid response and data parsing
 
-		for (let i = 0; i < books.length; i++) {
-			let card = document.createElement("article");
-			let image = document.createElement("img");
-			card.classList.add("clearfix");
-
-			image.setAttribute(
-				"alt",
-				'"' +
-					books[i].data["Nombre"] +
-					'"' +
-					", escrito por " +
-					books[i].data["Autor"] +
-					"."
-			);
-			image.setAttribute("src", books[i].cover);
-			card.appendChild(image);
-
-			for (key in books[i].data) {
-				let b = books[i].data;
-
-				if (b[key] != null) {
-					let span = document.createElement("span");
-					span.classList.add("span-bold");
-					span.textContent = key;
-
-					let p = document.createElement("p");
-					p.appendChild(span);
-					p.append(": " + b[key]);
-
-					card.appendChild(p);
-				}
-			}
-
-			for (key in books[i].links) {
-				let b = books[i].links;
-
-				if (b[key] != null) {
-					let spanTag = document.createElement("span");
-					spanTag.classList.add("span-bold");
-					spanTag.textContent = key;
-
-					let pTag = document.createElement("p");
-					pTag.appendChild(spanTag);
-
-					let aTag = document.createElement("a");
-					aTag.classList.add("link-black");
-					aTag.classList.add("link-slimed");
-					aTag.textContent = "Enlace externo";
-					aTag.setAttribute("href", b[key]);
-					pTag.append(": ");
-					pTag.appendChild(aTag);
-
-					card.appendChild(pTag);
-				}
-			}
-
-			document.querySelector("section.books").appendChild(card);
+		for (let i = 0; i < references.length; i++) {
+			let container = document.createElement('div');
+			container.innerHTML = `<button class="btn btn-dark" type="button" data-bs-placement="top" title="${i}" data-bs-toggle="collapse" data-bs-target="#${references[i].anchorName}" aria-expanded="false" aria-controls="${references[i].anchorName}">${references[i].buttonName}</button>`;
+			document.querySelector("#target").appendChild(container);
 		}
 	});
