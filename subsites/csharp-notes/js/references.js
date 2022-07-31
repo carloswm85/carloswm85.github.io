0a49db88050c3cf1a03e229d2a437b4da8f6483c
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-07-30 23:05:11 -0300

    simple update

--- subsites/csharp-notes/js/references.js
+++ subsites/csharp-notes/js/references.js
@@ -37,7 +37,7 @@ fetch(requestURL)
 			let links = references[i].links;
 
 			let div1 = document.createElement("div");
-			div1.classList.add("position-absolute", "left-50", "top-0", "mx-auto");
+			div1.classList.add("position-absolute", "start-0", "top-0");
 			div1.setAttribute("style", "min-height: 120px;");
 
 			let div2 = document.createElement("div");
