f489cb49ec11a3966c58875b021856e1f370900c
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2026-08-01 10:39:52 -0300

    feat: add diploma and certificate documents to education section

--- src/app/features/education-card/education-card.component.ts
+++ src/app/features/education-card/education-card.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { EducationItem } from '../../site-content';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-education-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [NgIf, MatCardModule, MatIconModule],
   templateUrl: './education-card.component.html',
   styleUrl: './education-card.component.scss',
 })
